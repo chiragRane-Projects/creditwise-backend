df3e008ee8fe391b18bc356b9fba07bc646ae8e5
Chirag Rane <beingchirag7@gmail.com>
2025-07-29 10:59:25 +0530

    Web App Complete

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from app.api.v1 import route
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="CreditWise API",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost",  # if you're using something else
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # 👈 Allow frontend dev origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(route.router, prefix="/api/v1/user", tags=["User"])
